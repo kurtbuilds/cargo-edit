3a5e817e18fce5e5b53435238139f67db9e4e0aa
Kurt Wolf <kurtwolfbuilds@gmail.com>
2021-12-02 17:32:43 -0500

    remove the extra dot on cargo-add with features

--- src/bin/add/main.rs
+++ src/bin/add/main.rs
@@ -97,7 +97,6 @@ fn print_msg(dep: &Dependency, section: &[String], optional: bool) -> Result<()>
     if let Some(f) = &dep.features {
         writeln!(output, " with features: {:?}", f)?;
     }
-    writeln!(output, ". ")?;
 
     if !&dep.available_features.is_empty() {
         writeln!(output, "{:>13}Available features: ", " ")?;
